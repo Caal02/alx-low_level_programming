b6e1643011d13d4745cbca940b2aad5de5bed07d
Caleb <caaleb02@gmail.com>
2022-07-02 03:11:24 -0700

    this the first output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-int main()
-
+ * main - prints the value given
+ * Return: value of 0
+ */
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-	    return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
