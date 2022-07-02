a879c15cd01c252ba484cdf64b1957f5ce872421
Caleb <caaleb02@gmail.com>
2022-07-02 13:07:40 -0700

    without using printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib>
+/**
+ * main - printing without usig puts and printf
+ * Return: value of 1
+ */
+int main(void)
+{
+	system("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
+
