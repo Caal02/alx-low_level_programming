d3565127ad8599fef85f89e0a4d16e003395ec43
Caleb <caaleb02@gmail.com>
2022-07-02 03:36:13 -0700

    print with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - prints the value given with printf
+* Return: value of 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
