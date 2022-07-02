312452446d4fc1be7735fc0aaec1efa52e70eba5
Caleb <caaleb02@gmail.com>
2022-07-02 03:54:01 -0700

    prit the size of vaiables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - print the size of variable types
+ * Return: the value of 0
+*/
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n
+	Size of an int: 4 byte(s)\n
+	Size of a long int: 4 byte(s)\n
+	Size of a long long int: 8 byte(s)\n
+	Size of a float: 4 byte(s)");
+	return (0);
+}
