3daad968628fced8fd5581ab560b9b020785e3bc
Caleb <caaleb02@gmail.com>
2022-07-03 11:07:06 -0700

    prints lower alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints lower alphabets
+ * Return: value of 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	putchar("%c", i);
+	}
+	putchar("\n");
+	void (0);
+}
