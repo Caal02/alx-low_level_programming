60f9a6af5ed1a8356be202fdba25e9656bd8cda4
Caleb <caaleb02@gmail.com>
2022-07-03 15:12:01 -0700

    comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints number in base 10
+ * Return: value of 0
+*/
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 48; a <= 57; a++)
+	{
+	putchar(a);
+	putchar(44);
+	putchar(32);
+	}
+	putchar(10);
+	return (0);
+}
