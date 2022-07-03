f2c191f248457217d5e9fcba837630b8bddd180c
Caleb <caaleb02@gmail.com>
2022-07-03 15:01:21 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints number in base 10
+ * Return: value of 0
+*/
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 48; a < 57; a++)
+	{
+	putchar(a);
+	}
+	for (b = 97; b <= 102; b++)
+	{
+	putchar(b);
+	}
+	putchar(10);
+	return (0);
+}
