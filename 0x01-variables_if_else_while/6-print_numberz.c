1ffe26090548ea86aa9537265614ca85428aeeb1
Caleb <caaleb02@gmail.com>
2022-07-03 14:43:01 -0700

    base 10 num

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints number in base 10 with putchar
+ * Return: value of 0
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+	putchar(a);
+	}
+	putchar(10);
+	return (0);
+}
