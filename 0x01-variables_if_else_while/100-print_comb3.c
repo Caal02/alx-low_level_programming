4e59acdaba9f1cf5336292a6600a8278ca613fb6
Caleb <caaleb02@gmail.com>
2022-07-03 15:59:59 -0700

    loop with loop

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints number in base 10
+ * Return: value of 0
+*/
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 49; b <= 57; b++)
+		{
+			if (b > a)
+			{
+			putchar(a);
+			putchar(b);
+				if (a != 56 || b != 57)
+				{
+				putchar(44);
+				putchar(32);
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
