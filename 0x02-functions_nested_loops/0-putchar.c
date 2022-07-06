fb272665662048108e28fa9ab43793cd1452bb85
Caleb <caaleb02@gmail.com>
2022-07-05 18:50:03 -0700

    how do

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,5 +7,12 @@
 */
 void print_sth(void)
 {
-	_putchar("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 }
