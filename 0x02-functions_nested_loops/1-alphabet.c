5b50d0e8f5c2e56b5f02c95114b4a70135dd1465
Caleb <caaleb02@gmail.com>
2022-07-06 16:31:53 -0700

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,11 @@
  * print_alphabet - prints alpha lower
  * Return: Always 0.
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int a;
 
-	for(a = 'a'; a <= 'z'; a++)
+	for (a = 'a'; a <= 'z'; a++)
 	{
 		_putchar(a);
 	}
