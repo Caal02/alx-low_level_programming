e59bcc9b2abebd19d9a0bb5a1d1dffeb89cd3b8e
Caleb <caaleb02@gmail.com>
2022-07-06 17:38:53 -0700

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks alpha character
+ * @n: it is a character
+ * Return: 1 0 -1 for signs + 0 -
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
