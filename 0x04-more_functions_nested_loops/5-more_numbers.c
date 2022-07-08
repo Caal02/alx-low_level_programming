9ce90a17fce30851ad586b724433a98aeaf81c04
Caleb <caaleb02@gmail.com>
2022-07-08 14:56:49 -0700

    commit for 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - chace for a digit
+ * Return: value 0
+ */
+void more_numbers(void)
+{
+	int a;
+	int c;
+
+	for (a = 0; a <= 10; a++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c > 9)
+			{
+				_putchar((c / 10) + '0');
+			}	
+			_putchar((c % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
