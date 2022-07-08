1ba9d86fcc2da3a71c84d0a7606fe704706b925c
Caleb <caaleb02@gmail.com>
2022-07-08 13:12:48 -0700

    commit for 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_numbers - chace for a digit
+ * Return: value 0
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		_putchar(c);
+	}
+		_putchar('\n');
+}
