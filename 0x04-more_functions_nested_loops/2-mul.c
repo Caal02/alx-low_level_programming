03cf819f0154aa42935a98a189d6274c3870948f
Caleb <caaleb02@gmail.com>
2022-07-08 12:45:56 -0700

    commit for 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *mul - chace for a digit
+ * @a: integer
+ * @b: integer
+ * Return: 1 if it is digit 0 if it's not
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
