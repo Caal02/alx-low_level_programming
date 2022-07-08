9405967cd9da9184d908a0501c50163365d0634d
Caleb <caaleb02@gmail.com>
2022-07-08 15:43:15 -0700

    commit for 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,11 +8,16 @@ void print_line(int n)
 {
 	int a;
 
-	if (n > 0)
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
 		for (a = 0; a < n; a++)
 		{
 			_putchar(95);
 		}
-	else
-		_putchar('\n');
+	_putchar('\n');
+	}
 }
