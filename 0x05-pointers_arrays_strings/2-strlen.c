29272499b0ceffeed67dd102ed72c4633d9b030c
Caleb <caaleb02@gmail.com>
2022-07-11 17:50:47 -0700

    tryit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_strlen - update value of pointer
+ *@s: integer vaue
+ *Return: zero
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	a = sizeof(char *s);
+	_putchar(a);
+
+}
