8d100d97134a902b9135fd713aba154aacc76b84
Caleb <caaleb02@gmail.com>
2022-07-12 15:33:40 -0700

    done

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,11 +9,10 @@ void print_array(int *a, int n)
 {
 	int f, i;
 
-	i = a[n];
 	for (f = 0; f < n - 1; f++)
 	{
-	_putchar(i);
-	_putchar(44 + '\0');
+	_putchar(a[n]);
+	_putchar(44 + ' ');
 	}
 	_putchar('\n');
 }
