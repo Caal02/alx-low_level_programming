ff22291457bd9712cfea09327205230dac857d8a
Caleb <caaleb02@gmail.com>
2022-07-14 13:47:19 -0700

    start string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * char *_strcat - cocnate two strings
+ * @dest: char type
+ * @src: char type
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	_putchar('dest' + 'scr');
+	return (dest);
+}
