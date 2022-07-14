fadcd51ea9300c6d62b75dce1b662e907a8279bf
Caleb <caaleb02@gmail.com>
2022-07-14 15:14:33 -0700

    this is try

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,17 +8,6 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int l = 0;
-	int i;
-
-	n = 1;
-	while (dest[l])
-		l++;
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[l] = src[i];
-		l += 1;
-	}
-	dest[l] = '\0';
+	strncat(dest, src, n);
 	return (dest);
 }
