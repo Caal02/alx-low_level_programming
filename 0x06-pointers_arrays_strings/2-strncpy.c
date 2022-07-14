b7252c4449a5bf35365947612f20b6a500304d60
Caleb <caaleb02@gmail.com>
2022-07-14 15:25:51 -0700

    this is try

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,23 +1,14 @@
 #include "main.h"
+#include <string.h>
 /**
  * _strncpy - cocnate two strings
  * @dest: char type
  * @src: char type
- * @n: int type
+ * @n: int
  * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int l = 0;
-	int i;
-
-	while (dest[l])
-		l++;
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[l] = src[i];
-		l += 1;
-	}
-	dest[l] = '\0';
+	strncpy(dest, src, n);
 	return (dest);
 }
