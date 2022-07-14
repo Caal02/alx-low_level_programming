addc128ff830398a96a1b93da11bf29b80ff7cf3
Caleb <caaleb02@gmail.com>
2022-07-14 14:38:25 -0700

    tring

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,22 @@
 #include "main.h"
 /**
- * char *_strcat - cocnate two strings
+ * _strcat - cocnate two strings
  * @dest: char type
  * @src: char type
  * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
-	_putchar(src);
+	int l = 0;
+	int i;
+
+	while (dest[l])
+		l++;
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[l] = src[i];
+		l += 1;
+	}
+	dest[l] = '\0';
 	return (dest);
 }
