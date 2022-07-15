0a0332c21a94bdd074fe91323044265935791683
Caleb <caaleb02@gmail.com>
2022-07-15 16:28:43 -0700

    for lets5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 include "main.h"
 /**
   * string_toupper - Changes all lowercase letters to uppercase
-  * @p: The string will be modified
+  * @x: The string will be modified
   * Return: char var
   */
 char *string_toupper(char *x)
@@ -12,7 +12,7 @@ char *string_toupper(char *x)
 	{
 		if (x[i] >= 97 && x[i] <= 122)
 			x[i] = x[i] - 32;
-			i++;
+	i++;
 	}
 	return (x);
 }
