8e638545f83927327f36be07a49b901b439691e6
Caleb <caaleb02@gmail.com>
2022-07-18 15:33:42 -0700

    for 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with constant byet
+ * @s: char type
+ * @b: char type
+ 8 @n: int type
+ * Return: value of s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < 0; x++)
+	{
+		s[x} = b;
+	}
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_memcpy - copies memory area 
+ * @dest: char type
+ * @src: char type
+ * @n: unsigned int
+ * Return: value of
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	
+	return ();
+}
