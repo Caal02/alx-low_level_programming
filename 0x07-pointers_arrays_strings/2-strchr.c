a48f9cad9c0b4a80ee1bd1b05372525c32fbd1cc
Caleb <caaleb02@gmail.com>
2022-07-19 13:49:49 -0700

    for 0

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,25 @@
 #include "main.h"
 /**
- *  - 
- * @
- * @
+ * _strchr - locates char string
+ * @s: char type
+ * @c: char type
  * Return: value of
  */
-
+char *_strchr(char *s, char c)
 {
+	int a = 0, b;
 
-	return ();
+	while (s[a])
+	{
+		a++;
+	}
+	for (b = 0; b <= a; b++)
+	{
+		if (c == s[b])
+		{
+		s += b;
+		return (s);
+		}
+	}
+	return ('\0');
 }
