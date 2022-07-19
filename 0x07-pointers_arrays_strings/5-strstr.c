2babf968bebae4fb7fc3399307adbe67d4313249
Caleb <caaleb02@gmail.com>
2022-07-19 14:20:28 -0700

    for 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,12 +1,30 @@
 #include "main.h"
 /**
- *  - 
- * @
- * @
- * Return: value of
- */
-
+  * _strstr - locate a substring
+  * @haystack: the string to search
+  * @needle: the string to find
+  *
+  * Return: char value
+  */
+char *_strstr(char *haystack, char *needle)
 {
+	int a = 0, b = 0;
 
-	return ();
+	while (haystack[a])
+	{
+		while (needle[b])
+		{
+			if (haystack[a + b] != needle[b])
+			{
+				break;
+			}
+			b++;
+		}
+		if (needle[b] == '\0')
+		{
+			return (haystack + a);
+		}
+		a++;
+	}
+	return ('\0');
 }
