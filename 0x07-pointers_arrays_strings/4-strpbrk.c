f70f14db194d1c2c2350c07eb17b221ba718a5f9
Caleb <caaleb02@gmail.com>
2022-07-19 14:15:28 -0700

    for 0

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,12 +1,28 @@
 #include "main.h"
 /**
- *  - 
- * @
- * @
- * Return: value of
- */
-
+  * _strpbrk - search a string for any of a set of bytes
+  * @s: source string
+  * @accept: accepted characters
+  *
+  * Return: the string since the first found accepted character
+  */
+char *_strpbrk(char *s, char *accept)
 {
+	int a = 0, b;
 
-	return ();
+	while (s[a])
+	{
+		b = 0;
+		while (accept[b])
+		{
+			if (s[a] == accept[b])
+			{
+				s += a;
+				return (s);
+			}
+			b++;
+		}
+		a++;
+	}
+	return ('\0');
 }
