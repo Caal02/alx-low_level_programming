554cff9117e4ea02126c366d4a42fd3239f0b675
Caleb <caaleb02@gmail.com>
2022-07-20 13:18:32 -0700

    Recurtion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts_recursion - prints string in new line
+ * @s: char type
+ */
+void _puts_recursion(char *s)
+{
+	char a[];
+	int i;
+	if i == 0;
+	{
+		return (0);
+	}
+	_putchar (a[i] + _put_recursion(char *s));
+	i++;
+	_putchar ('\n');
+}
