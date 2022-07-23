0971f64dc6e577249119ab843a0866a2c8c7cd98
Caleb <caaleb02@gmail.com>
2022-07-23 08:05:07 -0700

    Recurtion

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 /**
   * main - print name follwed by /n
   * @argc: int value
@@ -8,18 +8,11 @@
  */
 int main(int argc, char *argv[])
 {
-	int n1 = 0, n2 = 0;
-
-	if (argc == 3)
-	{
-		n1 = atoi(argv[1]);
-		n2 = atoi(argv[2]);
-		printf("%d\n", n1 * n2);
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	printf("%d", atio(argv[1]) * atio(argv[2]));
 	return (0);
 }
