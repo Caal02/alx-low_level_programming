1fbaec261aa0b66f230003b0bd369647dbb8829d
Caleb <caaleb02@gmail.com>
2022-07-22 19:37:22 -0700

    arg

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
   * main - print name follwed by /n
-  * @argv: char value
+  * @argc: int value
+  * @argv: char vaue
   * Return: value of zero
  */
-int main(char *argv[])
-{
+int main(int argc, char *argv[])
+{	
+	(void) argc;
 	printf("%s\n", *argv);
 	return (0);
 }
