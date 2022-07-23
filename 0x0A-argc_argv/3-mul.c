a84c5f73435794220b72281098c5c60f177a7c56
Caleb <caaleb02@gmail.com>
2022-07-22 19:46:10 -0700

    arg

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,7 +5,20 @@
   * @argv: char value
   * Return: value of zero
  */
-int main(void)
+int main(int argc, char *argv[])
 {
+	int n1 = 0, n2 = 0;
 
+	if (argc == 3)
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
 }
