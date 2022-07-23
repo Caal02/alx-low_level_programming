d9407cee460d5a60d224f08da8fa9e15974288db
Caleb <caaleb02@gmail.com>
2022-07-22 18:31:36 -0700

    trying static

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * _strcpy - locate a substring
+  * @haystack: the string to search
+  * @needle: the string to find
+  *
+  * Return: char value
+  */
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
