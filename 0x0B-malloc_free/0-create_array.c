3a2921eef6e2b7c5633c8fdd186473cd43a9d4c4
Caleb <caaleb02@gmail.com>
2022-07-25 14:40:40 -0700

    try for all

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 /**
  * create_array - creat arrayof char intalized with char
  * @size: unsigned int value
@@ -9,6 +10,15 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *p;
+	unsigned int i;
 
-	return (NULL);
+	p = (char *)malloc(sizeof(char) * size);
+	if (size == 0)
+		return (NULL);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		p[i] = c;
+		return (p);
 }
