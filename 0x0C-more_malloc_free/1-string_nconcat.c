4fd0e577a61e613cb6a4c54cf61ce10f62f1f34a
Caleb <caaleb02@gmail.com>
2022-07-28 14:27:09 -0700

    more malloc

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,15 @@
+#include <main.h>
+/**
+ * string_nconcat - function that concatenates two strings
+ * @s1: char value
+ * @s2: char value
+ * n: int value
+ * Return: string and Null if falls
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s = "Hellow";
+
+	return (s);
+}
+
