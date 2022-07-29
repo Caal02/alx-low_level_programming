248019a67fd596bbb6d5a2a95376d161bff99468
Caleb <caaleb02@gmail.com>
2022-07-29 13:54:01 -0700

    more malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,7 +4,12 @@
  * @b: int value
  * Return: pointer to allocated memory
  */
- void *malloc_checked(unsigned int b)
- {
-	b = 1;
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
 }
