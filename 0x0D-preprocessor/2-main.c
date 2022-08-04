8ac7cd064094afc82c4b64d46797dace6d284ad1
Caleb <caaleb02@gmail.com>
2022-08-04 09:20:06 -0700

    for size

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - prints the name of compiled file
+  * Return - zero value in success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
