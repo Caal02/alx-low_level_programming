6c5b60944581e66cf8ecd744717eecf6d4d93acd
Caleb <caaleb02@gmail.com>
2022-08-06 07:25:19 -0700

    struct header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+int _putchar(char c);
+
+/**
+  * struct dog - the structure has elements age,name & owner
+  * @name: name of the dog
+  * @age: age of the dog
+  * @owner: owenr of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
