e011472b4d9c75c50e79d821aab8891db4cedc52
Caleb <caaleb02@gmail.com>
2022-08-06 09:38:19 -0700

    struct initalize

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,12 +2,6 @@
 #define DOG_H
 
 int _putchar(char c);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
 /**
   * struct dog - the structure has elements age,name & owner
   * @name: name of the dog
@@ -20,5 +14,11 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+*void print_dog(struct dog *d);
+*
+*dog_t *new_dog(char *name, float age, char *owner);
+*void free_dog(dog_t *d);
 
 #endif
