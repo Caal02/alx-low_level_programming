82d798a9b85b3348d40c19fd09eaedd53a83067b
Caleb <caaleb02@gmail.com>
2022-08-06 10:30:34 -0700

    sturcture intia

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 /**
   * init_dog - initalize the variable of struct dog
   * @d: pointer
@@ -8,7 +9,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d != NULL)
+	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
+	}
+
 }
