07ec965cd65ee6c3a9be3d1e02f48930991f7afa
Caleb <caaleb02@gmail.com>
2022-08-22 12:31:08 -0700

    read0 i.o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <fcntl.h>
 #include "main.h"
-#include <unistd.h>
-
 /**
- * read_textfile - returns the actual number of letters it could read and print
- * @filename: file to read and print
- * @letters: number of letters to print
- * Return: the number or letters
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ * Return: the number of letters printed, or 0 if it failed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-
-	int fd, sz;
+	int fd;
+	int i, y;
 	char *buf;
 
-	if (filename == NULL)
-		return (0);
-	buf = malloc(letters * sizeof(char));
-	if (buf == NULL)
+	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (fd < 0)
 		return (0);
-	sz = write(STDOUT_FILENO, buf, read(fd, buf, letters));
-	if (sz == -1)
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
 		return (0);
+	i = read(fd, buf, letters);
+	if (i < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	buf[i] = '\0';
 	close(fd);
+	y = write(STDOUT_FILENO, buf, i);
+	if (y < 0)
+	{
+		free(buf);
+		return (0);
+	}
 	free(buf);
-	return (sz);
-
-
+	return (y);
 }
