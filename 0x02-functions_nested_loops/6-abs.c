b54b8696d32735d2c2c02264b41cba1cfe7d903f
Caleb <caaleb02@gmail.com>
2022-09-03 01:14:53 -0700

    absolut valute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  *_abs - prints absolut value
+  *@int: param is value to be absolute
+  *Return: int valure
+ */
+int _abs(int a)
+{
+	if (a < 0)
+		return (-a);
+	else
+		return (a);
+}
