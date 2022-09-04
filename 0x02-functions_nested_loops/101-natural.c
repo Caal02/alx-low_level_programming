0244cc6c6c128a14433d1e5c5591976ab66915ed
Caleb <caaleb02@gmail.com>
2022-09-04 09:28:11 -0700

    multi of  3&5 sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  *main - calulat multiple and sum of 3&5
+  *Return: no value
+ */
+
+int main(void);
+{
+	int i, a, b, c;
+
+	for (i = 1; i < = 1024; i++)
+	{
+		if (i / 3 == 0)
+			a = i;
+		if (i / 5 == 0)
+			b = i;
+		c = a + b;
+	}
+		printf("%d\n", c);
+	return (0);
+}
