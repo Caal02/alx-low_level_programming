23440616ce5ca5711088a49bf8ee13e87571b49d
Caleb <caaleb02@gmail.com>
2022-09-04 06:56:19 -0700

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
-  *print_last_digit - prints absolut value
+  *add - prints absolut value
   *@a: param is value to be absolute
+  *@b: added value
   *Return: int valure
  */
 int add(int a, int b)
 {
-	int c;
-
 	c = a + b;
 	return (c);
 }
